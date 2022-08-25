246ef62b08a11961cd2eda9d00a8a8ddd37b92ce
S <30459885+tacticstactics@users.noreply.github.com>
2022-08-25 15:59:41 +0900

    1

---
+++ PythonApplication1/PythonApplication1/Fresnel-equations_main.py
@@ -0,0 +1,36 @@
+
+
+#Fresnel-equations_main.py
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import Fresnel-equations_def
+
+param = 0.001
+m = 512
+
+wlcol,PTetacol,PRetacol,Etphasecol,Erphasecol = Fresnel-equations_def.proc1(param,m)
+
+print('')
+print('Etalon_Phase_main.py')
+print('')
+
+fig = plt.figure(figsize = (10,6), facecolor='lightblue')
+
+
+ax1 = fig.add_subplot(2, 2, 1)
+ax2 = fig.add_subplot(2, 2, 2, sharey=ax1)
+ax3 = fig.add_subplot(2, 2, 3)
+ax4 = fig.add_subplot(2, 2, 4, sharey=ax3)
+
+ax1.plot(wlcol,PTetacol)
+ax2.plot(wlcol,PRetacol)
+ax3.plot(wlcol,Etphasecol)
+ax4.plot(wlcol,Erphasecol)
+
+#ax3.plot(wlcol,np.real(Signalcol))
+
+
+plt.show()
